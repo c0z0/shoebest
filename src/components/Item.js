ba88a81cf719d28fb1ac0e37abd28301a58f298f
Cosmin Serdean <cosmoserdean@gmail.com>
2017-04-02 13:08:41 +0300

    Worked on cart prototype

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 import "./Item.sass"
 
@@ -7,9 +8,13 @@ class Item extends Component {
 		return (
 			<div className="col-md-3 col-sm-4 col-xs-12">
 				<div className="rectangle">
-					<img src="http://www.u-man.ro/resources/7aa8881640166a004a1a823dc18eede1/IMG_1723_sm.jpg" alt="Product" onClick={this.props.toggleInfo}/>
+					<Link to="/details/vans">
+						<img src="http://www.u-man.ro/resources/7aa8881640166a004a1a823dc18eede1/IMG_1723_sm.jpg" alt="Product" onClick={this.props.toggleInfo}/>
+					</Link>
 					<div className="productInfo">
-						<h4>Product title <span className="price">200 RON</span></h4>
+						<Link to="/details/vans">
+							<h4>Product title <span className="price">200 RON</span></h4>
+						</Link>
 					</div>
 				</div>
 			</div>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 
+import { Link } from 'react-router-dom'
+
 import './Cart.sass'
 
 export default class Cart extends Component {
 	render() {
 		return (
 			<div className="cart">
-				<h3 className="title">
-					Shoe Best Cart
-				</h3>
+				<Link to="/" className="exit"><i className="fa fa-remove"></i></Link>
+				<div className="container">
+					<h2 className="title">
+						Cart
+					</h2>
+				</div>
+				<div className="items">
+					<Item/>
+					<Item/>
+					<Item/>
+				</div>
 			</div>
 			);
 	}
 }
+
+const Item = (props) => (
+		<div className="item">
+			<h3>Vans Old Skool</h3>
+			<h3 className="price"><i className="fa fa-remove"></i></h3>
+
+			<h3 className="price">350 RON</h3>
+		</div>
+	)
